85666d2016414a0fafa2b229dac3d369b4324f8e
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2026-09-19 21:11:01 +0300

    Fix ebook model

--- fawrii2/src/Ebook.java
+++ fawrii2/src/Ebook.java
@@ -1,11 +1,17 @@
-import java.util.HashMap;
-
 public class Ebook extends Book {
     private String fileType;
+
     public Ebook() {}
-    public Ebook(String title, String isbn, int year, double price,String publishDate,int quantity) {
-        super(isbn,title,price,year,publishDate,quantity);
+
+    public Ebook(String title, String isbn, int year, double price, String publishDate, int quantity) {
+        super(isbn, title, price, year, publishDate, quantity);
+    }
+
+    public Ebook(String title, String isbn, int year, double price, String publishDate, int quantity, String fileType) {
+        this(title, isbn, year, price, publishDate, quantity);
         this.fileType = fileType;
     }
 
+    public String getFileType() { return fileType; }
+    public void setFileType(String fileType) { this.fileType = fileType; }
 }
